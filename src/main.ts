c8debd1039b1b82c615562bbe2b8fa0e3e633775
Nikita Shilin <shilin.nick@yandex.ru>
2022-07-09 13:53:33 +0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('cards', app, document);
-
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
