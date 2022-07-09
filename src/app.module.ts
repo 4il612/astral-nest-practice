8513d9dd690fd0bbd4c26b3f9c93d4de36c4bd2c
Nikita Shilin <shilin.nick@yandex.ru>
2022-07-09 13:29:26 +0300

    add: local sqlite db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { CardsModule } from './cards/cards.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
-    database: ':memory:',
+    database: 'base.sqlite',
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
   }), CardsModule],
